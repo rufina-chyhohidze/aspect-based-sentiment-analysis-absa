552e31c6e4ceeec70e4f4d9a9101522a238bdae4
PhChevico <philipe.domingues@hotmail.com>
2025-10-02 11:59:37 +0200

    starting imp2

--- src/llm_absa.py
+++ src/llm_absa.py
@@ -1,1 +1,7 @@
 #implementation2
+
+
+
+classifier = pipeline("text-classification", model="yangheng/deberta-v3-base-absa-v1.1")
+sentence = "The food was exceptional, although the service was a bit slow."
+
